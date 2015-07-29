bdcea284b4d6d6712b4d1349c0734746597bd6dc
jimot <jimot@users.noreply.github.com>
2015-07-29 11:57:24 +0200

    Ajout d'une dernière fonction au fichier .Js
    fonction key_detect_calc

--- calculatrice.js
+++ calculatrice.js
@@ -111,3 +111,61 @@ function initialiser_calc(id)
         document.getElementById(id+'_resultat').focus();
         return true;
 }
+function key_detect_calc(id,evt)
+{
+        if((evt.keyCode>95) && (evt.keyCode<106))
+        {
+                var nbr = evt.keyCode-96;
+                add_calc(id,nbr);
+        }
+        else if((evt.keyCode>47) && (evt.keyCode<58)
+        {
+                var nbr = evt.keyCode-48;
+                add_calc(id,nbr);
+        }
+        else if(evt.keyCode==107)
+        {
+                f_calc(id,'+');
+        }
+        else if(evt.keyCode==109)
+        {
+                f_calc(id,'-');
+        }
+        else if(evt.keyCode==106)
+        {
+                f_calc(id,'*');
+        }
+        else if(evt.keyCode==111)
+        {
+                f_calc(id,'');
+        }
+        else if(evt.keyCode==110)
+        {
+                add_calc(id,'.');
+        }
+        else if(evt.keyCode==190)
+        {
+                add_calc(id,'.');
+        }
+        else if(evt.keyCode==188)
+        {
+                add_calc(id,'.');
+        }
+        else if(evt.keyCode==13)
+        {
+                f_calc(id,'=');
+        }
+        else if(evt.keyCode==46)
+        {
+                f_calcid,'ce');
+        }
+        else if(evt.keyCode==8)
+        {
+                f_calc(id,'nbs');
+        }
+        else if(evt.keyCode==27)
+        {
+                f_calc(id,'ce');
+        }
+        return true;
+}
